c5ca5f6a551f5bcd3b38c9171abada72835a1840
SHADOW-465 <kumaarsk465@gmail.com>
2025-09-18 20:09:21 +0530

    Delete genius directory

--- genius/src/app/(auth)/layout.tsx
+++
@@ -1,9 +0,0 @@
-const AuthLayout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <main className="flex items-center justify-center h-screen">
-      {children}
-    </main>
-  );
-};
-
-export default AuthLayout;
