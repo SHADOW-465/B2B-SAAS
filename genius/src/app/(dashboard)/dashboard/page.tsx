a1c4ea7db7cc71208aeb6174b8f9387bb369b7d2
SHADOW-465 <kumaarsk465@gmail.com>
2025-09-18 13:55:15 +0530

    Add files via upload

---
+++ genius/src/app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,10 @@
+const DashboardPage = () => {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold">Dashboard</h2>
+      <p>Welcome to your dashboard.</p>
+    </div>
+  )
+}
+
+export default DashboardPage;
